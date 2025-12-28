24af0a32351839b5b635699e34a241ee8a4c6c17
bsharabi <bsharabi95@gmail.com>
2025-12-28 15:39:22 +0200

    up

---
+++ src/Week07/JUNIT/CalculatorTest.java
@@ -0,0 +1,25 @@
+package Week07.JUNIT;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorTest {
+
+    @Test
+    void add() {
+    }
+
+    @Test
+    void subtract() {
+        assertEquals(1,1);
+    }
+
+    @Test
+    void multiply() {
+    }
+
+    @Test
+    void divide() {
+    }
+}
